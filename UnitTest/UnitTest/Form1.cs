b299d761a32c9c0044cbc0652e6cabdf5749c975
park_j <park_j@subway.com>
2017-05-10 16:42:44 -0700

    Add Button

--- UnitTest/UnitTest/Form1.cs
+++ UnitTest/UnitTest/Form1.cs
@@ -15,5 +15,10 @@ namespace UnitTest
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("asdf");
+        }
     }
 }
