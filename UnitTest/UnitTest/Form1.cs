a47746238e60221ffc4434452d363e308bf335ef
psgzzang <park_j@subway.com>
2017-05-17 10:50:59 -0700

    test

--- UnitTest/UnitTest/Form1.cs
+++ UnitTest/UnitTest/Form1.cs
@@ -19,7 +19,7 @@ namespace UnitTest
         private void button1_Click(object sender, EventArgs e)
         {
             MessageBox.Show("asdf");
-            sdfgfdsgdf
+            sdfgfdsgdfaaa
         }
     }
 }
